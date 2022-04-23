e5fe2ff12033ca2000243675f6b3680e31598278
spees10 <Spees.lm@gmail.com>
2022-04-23 23:57:40 +0200

    product image

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class APIService {
   constructor(private http: HttpClient) { }
 
   getAllProducts() {
-    return this.http.get<any>("https://fakestoreapi.com/products").pipe(map((res: any) => {
+    return this.http.get<any>(`${environment.apiBaseUrl}/products`).pipe(map((res: any) => {
       return res;
     }));
   }
