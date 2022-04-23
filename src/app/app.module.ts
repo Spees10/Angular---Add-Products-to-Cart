8f5031be70d012b5c8e416742a521f77f8f32739
spees10 <Spees.lm@gmail.com>
2022-04-23 21:39:53 +0200

    Navbar Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { CartComponent } from './Components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
