d01e46872aa8d77fb8b4acd59e08d36e9004c236
spees10 <Spees.lm@gmail.com>
2022-04-24 15:01:13 +0200

    product finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './Components/cart/cart.component';
+import { ProductsComponent } from './Components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Products', pathMatch: 'full' },
+  { path: 'Products', component: ProductsComponent },
+  { path: 'Cart', component: CartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
